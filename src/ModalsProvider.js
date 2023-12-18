e59c5f465a037c7cc19cfe2333b25e17d61485bb
heejin.park <lghj.park@lge.com>
2023-12-19 08:29:08 +0900

    Modal Context 제공
    Change-Id: Iecbe050b7a511b341b8dce6806851225af362ad7

---
+++ src/ModalsProvider.js
@@ -0,0 +1,33 @@
+import { useState, useMemo } from "react";
+import { ModalsDispatchContext, ModalsStateContext } from "./ModalsContext";
+
+const ModalsProvider = ({ children }) => {
+  // openModals는 [Component, props]의 리스트 형태
+  const [openModals, setOpenedModals] = useState([]);
+
+  const open = (Component, props) => {
+    setOpenedModals((modals) => {
+      return [...modals, { Component, props }];
+    });
+  };
+
+  const close = (Component) => {
+    setOpenedModals((modals) => {
+      return modals.filter((modal) => {
+        return modal.Component !== Component;
+      });
+    });
+  };
+
+  const dispatch = useMemo(() => ({ open, close }), []);
+
+  return (
+    <ModalsStateContext.Provider value={openModals}>
+      <ModalsDispatchContext.Provider value={dispatch}>
+        {children}
+      </ModalsDispatchContext.Provider>
+    </ModalsStateContext.Provider>
+  );
+};
+
+export default ModalsProvider;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ReactModal from "react-modal";
 import App from "./App";
+import ModalsProvider from "./ModalsProvider";
 
 ReactModal.setAppElement("#root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ModalsProvider>
+      <App />
+    </ModalsProvider>
   </React.StrictMode>
 );
